869ec7636e465f887cf2cc25a9f2c416f114a3a1
Takayuki ORITO <iori.ayane@gmail.com>
2017-01-31 22:42:56 +0900

    add style change example

---
+++ src/StyleChangeExample/ChangeByCPP/main.cpp
@@ -0,0 +1,18 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QtQuickControls2/QQuickStyle>
+
+int main(int argc, char *argv[])
+{
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QGuiApplication app(argc, argv);
+
+//  QQuickStyle::setStyle("Default");
+  QQuickStyle::setStyle("Material");
+//  QQuickStyle::setStyle("Universal");
+
+  QQmlApplicationEngine engine;
+  engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+
+  return app.exec();
+}
